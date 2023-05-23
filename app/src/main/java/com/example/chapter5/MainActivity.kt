291a85c342a8d1848f88e1cbec176fd645299b07
bomblock <ekwotmd@gmail.com>
2023-05-23 23:18:18 +0900

    day11

--- app/src/main/java/com/example/chapter5/MainActivity.kt
+++ app/src/main/java/com/example/chapter5/MainActivity.kt
@@ -2,10 +2,36 @@ package com.example.chapter5
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import com.example.chapter5.databinding.ActivityMainBinding
+import java.lang.StringBuilder
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private val firstNumberText = StringBuilder("")
+    private val secondNumberText = StringBuilder("")
+    private val operatorText = StringBuilder("")
+    //todo 값을 받고 다음 값을 받은 다음 연산자 을 받는 구조 로 진행
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+    fun numberClicked(view: View) {
+        val nuberString = (view as? Button)?.text.toString() ?: ""
+        val nuberText = if(operatorText.isEmpty()) firstNumberText
+    }
+    fun clearClicked(view: View) {
+
+    }
+    fun equslClicked(view: View) {
+
+    }
+    fun operatorClicked(view: View) {
+
     }
 }
